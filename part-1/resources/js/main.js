ca50cc6591050c1b0158d8420bfb6285ba89d995
Zahid <zahidwp20@gmail.com>
2021-12-19 18:36:48 +0600

    New File

---
+++ part-1/resources/js/main.js
@@ -0,0 +1,16 @@
+var containerEl = document.querySelector('.portfolio-filter');
+
+var mixer = mixitup(containerEl, {
+    animation: {
+        duration: 100
+    }
+});
+function openNav() {
+    document.getElementById("myNav").style.width ="100%";
+
+
+}
+
+function openNav() {
+    document.getElementById("closeNav").style.width ="0";
+}
